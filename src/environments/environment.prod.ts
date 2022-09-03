7366cdee6220b69fc1d56eb3078bc5f3ab07c61e
phmiranda <pehhagah.1607@gmail.com>
2022-09-03 20:08:05 -0300

    First commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  /* endereços de serviços */
+  apiUrl: 'https://api.album.dominio.com.br',
+  apiSso: 'https://api.sso.dominio.com.br',
+
+  /* configuração da aplicação */
+  production: true,
+};
